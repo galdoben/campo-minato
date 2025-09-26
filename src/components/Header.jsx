003dd2ad4d0c79d05ec17471e7665dea42602454
galdoben <galdobenedetta@gmail.com>
2025-09-26 16:53:18 +0200

    feat: add Header component with title and instructions

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>💩 Campo Minato 💩</h1>
+      <p>Tocca le caselle ma evita le m*rd*ne! </p>
+    </header>
+  );
+};
+
+export default Header;
